5394e1d40ea87131809b3764aa8f2d83b603207c
Guilherme Bomfim Dev <guigui_bsg@hotmail.com>
2024-06-24 18:14:54 -0300

    Layout pagina de login e cadastro
    - HTML: Login e cadastro
    - CSS: Login e cadastro
    - Node.js: Adicionada validação para verificar no cadastro se as senhas colocadas conferem

--- Backend/server/routes/user.js
+++ Backend/server/routes/user.js
@@ -2,9 +2,14 @@ const express = require('express');
 const router = express.Router();
 
 router.post('/register', (req, res) => {
-    const { name, email, password } = req.body;
-    console.log(`Received data - Name: ${name}, Email: ${email}, Password: ${password}`);
-    res.json({ message: 'User registered successfully', data: { name, email, password } });
+    const { name, email, password, passwordRepeat } = req.body;
+    if (password == passwordRepeat) {
+        console.log(`Received data - Name: ${name}, Email: ${email}, Password: ${password}, PasswordRepeat: ${passwordRepeat}`,);
+        res.json({ message: 'User registered successfully', data: { name, email, password, passwordRepeat } });
+    } else {
+        console.log(`Received data - Name: ${name}, Email: ${email}, Password: ${password}, PasswordRepeat: ${passwordRepeat}`,);
+        res.json({ message: 'User not registered. Password is not same', data: { name, email, password, passwordRepeat } });
+    }
 });
 
 module.exports = router;
