5c3023d7b01a78acb566572377016b0074b97182
Guilherme Bomfim Dev <guigui_bsg@hotmail.com>
2024-06-24 18:44:02 -0300

    Melhoria nas validações
    - Verifica:
           - Tamanho do nome
           - Se o e-mail é válido
           - Tamanho da senha
    - Caso o erro seja desconhecido, retorna com a mensagem certa

--- Backend/server/routes/user.js
+++ Backend/server/routes/user.js
@@ -1,14 +1,24 @@
 const express = require('express');
+const { body, validationResult } = require('express-validator');
 const router = express.Router();
 
-router.post('/register', (req, res) => {
+router.post('/register', [
+    body('name').isLength({ min: 5}).withMessage('Nome é obrigatório'),
+    body('email').isEmail().withMessage('Digite um email válido'),
+    body('password').isLength({ min: 6}).withMessage('Senha deve ter mais de 6 caracteres') 
+], (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
     const { name, email, password, passwordRepeat } = req.body;
     if (password == passwordRepeat) {
         console.log(`Received data - Name: ${name}, Email: ${email}, Password: ${password}, PasswordRepeat: ${passwordRepeat}`,);
         res.json({ message: 'User registered successfully', data: { name, email, password, passwordRepeat } });
     } else {
         console.log(`Received data - Name: ${name}, Email: ${email}, Password: ${password}, PasswordRepeat: ${passwordRepeat}`,);
-        res.json({ message: 'User not registered. Password is not same', data: { name, email, password, passwordRepeat } });
+        res.json({ message: 'Usuário não registrado. As senhas estão diferentes', data: { name, email, password, passwordRepeat } });
     }
 });
 
